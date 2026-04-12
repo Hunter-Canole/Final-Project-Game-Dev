d85647f689463cf7f404fe4a549ab4b20fc62053
Hunter Canole <158322496+Hunter-Canole@users.noreply.github.com>
2026-04-12 13:31:55 -0500

    player logic

---
+++ final-project-game-dev/player.gd
@@ -0,0 +1,73 @@
+extends CharacterBody2D
+
+
+@export var SPEED = 300.0
+@export var JUMP_VELOCITY = -400.0
+@export var MAX_COYOTE_FRAMES = 100.0
+@export var timer_delay = 0.5
+@export var MAX_INPUT_BUFFER_FRAMES = 100
+
+
+var coyote_frames = 0
+var input_buffer_frames = 0;
+var believe_in_gravity = false
+var is_jumping = 0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+func _ready():
+	$AnimatedSprite2D.animation = 'idle'
+	$AnimatedSprite2D.play()
+
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+		#coyote_frames +=1
+		if $WileTimer.is_stopped():
+			$WileTimer.start(timer_delay)
+		if Input.is_action_just_pressed('ui_accept'):
+			input_buffer_frames = MAX_INPUT_BUFFER_FRAMES
+		if input_buffer_frames > 0 :
+			input_buffer_frames -= 1
+	else:
+		coyote_frames = 0
+		is_jumping = false
+		believe_in_gravity = false
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") or input_buffer_frames > 0:
+		if is_jumping == false and not believe_in_gravity:
+		#coyote_frames < MAX_COYOTE_FRAMES:
+			velocity.y = JUMP_VELOCITY
+			is_jumping = true
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = move_toward(velocity.x, direction * SPEED, 0.05*SPEED)
+		$AnimatedSprite2D.animation = 'Run'
+		$AnimatedSprite2D.flip_h = velocity.x < 0
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		$AnimatedSprite2D.animation = 'Idle'
+	
+	if not is_on_floor():
+		$AnimatedSprite2D.animation = 'Hurdle'
+	move_and_slide()
+
+func ow():
+	queue_free()
+
+func bounce():
+	velocity.y = JUMP_VELOCITY/1.5
+
+func set_pos(pos):
+	position=pos
+
+
+func _on_wile_timer_timeout() -> void:
+	believe_in_gravity = true
