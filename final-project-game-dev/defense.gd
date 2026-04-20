117632878bdb5441a3605b1953364f4a821e4f62
Hunter Canole <158322496+Hunter-Canole@users.noreply.github.com>
2026-04-19 21:39:47 -0500

    Defensive guy might work

--- final-project-game-dev/defense.gd
+++ final-project-game-dev/defense.gd
@@ -3,7 +3,8 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
-
+signal hit_player
+var is_diving = false
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -13,13 +14,19 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 	
 func _ready():
-	$AnimatedSprite2D.animation = 'idle'
-	$AnimatedSprite2D.play()
+	if is_diving == false:
+		$AnimatedSprite2D.animation = 'idle'
+		$AnimatedSprite2D.play()
 
 func _on_hitbox_body_entered(body: Node2D) -> void:
 	hit_player.emit()
 
 
 func _on_hurtbox_body_entered(body: Node2D) -> void:
+	is_diving = true
+	$Hitbox_Normal.disabled = true
+	$Hitbox_Diving.disabled = false
+	$Hurtbox.disabled = true
 	$AnimatedSprite2D.animation = 'diving'
 	$AnimatedSprite2D.play()
+	
