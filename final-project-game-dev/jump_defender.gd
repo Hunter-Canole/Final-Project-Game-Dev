76b4e0922e8857d07cc16312277ea85596515a80
Hunter Canole <158322496+Hunter-Canole@users.noreply.github.com>
2026-04-26 14:06:12 -0500

    jumping defender works

--- final-project-game-dev/jump_defender.gd
+++ final-project-game-dev/jump_defender.gd
@@ -6,31 +6,44 @@ var is_tackling = false
 
 func _ready():
 	starting_position = global_position
-	$AnimatedSprite2D.animation = "Idle"
-	$AnimatedSprite2D.play()
+	$AnimatedSprite2D.play("Idle")
 	jump_loop()
+
 func jump_loop():
 	while true:
 		await get_tree().create_timer(2.0).timeout
-		jump()
+		if is_on_floor() and not is_tackling:
+			jump()
+
 func jump():
-	if is_on_floor() and not is_tackling:
-		velocity.y = -600
-		$AnimatedSprite2D.animation = "Jumping"
-		
+	velocity.y = -600
+
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+	
 	move_and_slide()
 
+	if is_tackling:
+		return 
+
+	# Better logic: Use velocity to determine if we are airborne
+	if not is_on_floor() or abs(velocity.y) > 0:
+		$AnimatedSprite2D.animation = "Jumping"
+	else:
+		$AnimatedSprite2D.animation = "Idle"
+	
+	$AnimatedSprite2D.play()
+
 func _on_hitbox_body_entered(body: Node2D) -> void:
-	if is_tackling: return 
+	if is_tackling: 
+		return 
 	is_tackling = true
 	$Hitbox/CollisionShape2D.set_deferred("disabled", true)
-	$AnimatedSprite2D.play("tackling")
 	hit_player.emit()
-	await get_tree().create_timer(1.0).timeout
-	$AnimatedSprite2D.play("idle")
+	$AnimatedSprite2D.play("tackling")
+	await get_tree().create_timer(0.8).timeout 
 	is_tackling = false
+	global_position = starting_position
 	$Hitbox/CollisionShape2D.set_deferred("disabled", false)
 	
